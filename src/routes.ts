c77567904ad48b031035232dbb4d9c1895fd8d02
HIKARO CARVALHO <81428096+hikarocarvalho@users.noreply.github.com>
2022-03-20 20:37:53 -0300

    feat(class): from class 16 at class 21

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,23 @@
 import { Router, Request, Response } from "express";
-import { CreateUserController } from "./controllers/CreateUserController";
+import { CreateUserController } from "./controllers/userControllers/CreateUserController";
+import { GetAllUserController } from "./controllers/userControllers/GetAllUserController";
+import { UpdateUserController } from "./controllers/userControllers/UpdateUserController";
+import { DeleteUserController } from "./controllers/userControllers/DeleteUserController";
+
 const router = Router();
 const createUserController = new CreateUserController();
+const getAllUserController = new GetAllUserController();
+const updateUserController = new UpdateUserController();
+const deleteUserController = new DeleteUserController();
 
 router.get("/", (request: Request, response: Response) => {
   return response.json({ message: "Welcome to DIO API" });
 });
 
-router.post("/users", createUserController.handle);
+router
+  .post("/user", createUserController.handle)
+  .get("/user", getAllUserController.handle)
+  .patch("/user/:id", updateUserController.handle)
+  .delete("/user/:id", deleteUserController.handle);
 
 export { router };
